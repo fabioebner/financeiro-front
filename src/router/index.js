239225fe4a16d962b55bfe6a465f4a1ac9434aad
Fabio Ebner <fabioebner@gmail.com>
2018-04-18 10:23:29 -0300

    separando login em rota, e tela principal basica

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,20 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Login from '@/components/Login';
+import Principal from '@/components/Principal';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
+      name: 'Principal',
+      component: Principal,
+    },
+    {
+      path: '/login',
       name: 'Login',
       component: Login,
     },
